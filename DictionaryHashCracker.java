316efc29754e75b76ad7dfc1743551d9103800b2
nassour04 <abdelsalammhtnassour@gmail.com>
2026-07-16 22:16:03 +0000

    Modification DICO premier version

--- DictionaryHashCracker.java
+++ DictionaryHashCracker.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DictionaryHashCracker implements HashCracker {
 
     private static final String DICTIONARY = "dictionary.txt";
@@ -21,6 +25,7 @@ public class DictionaryHashCracker implements HashCracker {
                     break;
                 }
             }
+            scanner.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Impossible de lire le fichier de dictionnaire: " + DICTIONARY, e);
         }
